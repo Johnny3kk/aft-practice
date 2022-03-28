19eb6f4e3fd0e3e52b58bd65bf562e96abc9c6d0
Johnny3kk <some@mail.ru>
2022-03-29 01:02:53 +0300

    maven setup+;

--- src/main/java/ru/ibs/framework/utils/MyAllureListener.java
+++ src/main/java/ru/ibs/framework/utils/MyAllureListener.java
@@ -1,15 +1,13 @@
 package ru.ibs.framework.utils;
 
 import io.qameta.allure.Attachment;
-import io.qameta.allure.junit5.AllureJunit5;
 import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.runner.notification.RunListener;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import ru.ibs.framework.managers.DriverManager;
 
-public class MyAllureListener extends RunListener implements AfterTestExecutionCallback {
+public class MyAllureListener implements AfterTestExecutionCallback {
 
   @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
   public byte[] getScreenshot() {
