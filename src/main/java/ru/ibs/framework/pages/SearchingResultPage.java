c04c01c266364701511bb2b43fd603810a7ecc0f
Johnny3kk <some@mail.ru>
2022-03-30 10:51:49 +0300

    filling shopping cart+;

--- src/main/java/ru/ibs/framework/pages/SearchingResultPage.java
+++ src/main/java/ru/ibs/framework/pages/SearchingResultPage.java
@@ -24,6 +24,9 @@ public class SearchingResultPage extends BasePage {
   @FindBy(xpath = "//span[contains(text(), 'NFC')]/../..")
   private WebElement nfcCheck;
 
+  @FindBy(xpath = "//div[contains(text(), 'Дальше')]")
+  private WebElement nextPageBtn;
+
   @FindBy(xpath = "//a[contains(@class, 'tile-hover-target')]/span")
   private List<WebElement> foundProductList;
 
@@ -38,7 +41,7 @@ public class SearchingResultPage extends BasePage {
     scrollToElementJs(highRateCheck.findElement(By.xpath("./../../..")));
     waitUntilElementToBeVisible(highRateCheck);
     waitUntilElementToBeClickable(highRateCheck).click();
-//    waitUntilElementToBeClickable(getHeader().getLogo());
+    //    waitUntilElementToBeClickable(getHeader().getLogo());
     return this;
   }
 
@@ -70,7 +73,8 @@ public class SearchingResultPage extends BasePage {
   }
 
   public SearchingResultPage fillShoppingCart() {
-        String string = "./../../../..//button/span[not(contains(@class, 'ui-d8')) and not(contains(@class, 'ui-e8'))]";
+    String string =
+        "./../../../..//button/span[not(contains(@class, 'ui-d8')) and not(contains(@class, 'ui-e8'))]";
     for (int i = 0; i < foundProductList.size(); i++) {
       if (i % 2 != 0) {
         if (hasXpath(foundProductList.get(i), string)) {
@@ -83,11 +87,15 @@ public class SearchingResultPage extends BasePage {
                           .findElement(
                               By.xpath(
                                   "./../../../..//span[@style='color: var(--ozTextPrimary);']"))
-                          .getText().trim().replaceAll("[^0-9]", ""))));
+                          .getText()
+                          .trim()
+                          .replaceAll("[^0-9]", ""))));
           foundProductList
-                  .get(i)
-                  .findElement(
-                          By.xpath("./../../../..//button/span[not(contains(@class, 'ui-d8')) and not(contains(@class, 'ui-e8'))]")).click();
+              .get(i)
+              .findElement(
+                  By.xpath(
+                      "./../../../..//button/span[not(contains(@class, 'ui-d8')) and not(contains(@class, 'ui-e8'))]"))
+              .click();
           if (productManager.getProductList().size() == 8) {
             break;
           }
@@ -95,6 +103,19 @@ public class SearchingResultPage extends BasePage {
         }
       }
     }
+    if (productManager.getProductList().size() < 8) {
+      nextSearchPage();
+      fillShoppingCart();
+    }
     return this;
+  }
+
+  public void nextSearchPage() {
+    scrollToElementJs(nextPageBtn.findElement(By.xpath("./../../../../../../..")));
+    waitUntilElementToBeClickable(nextPageBtn).click();
+    foundProductList =
+        driverManager
+            .getDriver()
+            .findElements(By.xpath("//a[contains(@class, 'tile-hover-target')]/span"));
   }
 }
